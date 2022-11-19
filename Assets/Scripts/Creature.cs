712ea9e7d524a0e053eb4f3093ca75713c02cae9
MentIin <81743613+MentIin@users.noreply.github.com>
2022-11-19 17:27:05 +0200

    Base Creature script

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public int health { get; private set; }
+
+    protected virtual void Update()
+    {
+        if (health <= 0) Die();
+    }
+
+    virtual public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+    
+
+    virtual public void GetDamage(int damage)
+    {
+        health -= damage;
+    }
+}
