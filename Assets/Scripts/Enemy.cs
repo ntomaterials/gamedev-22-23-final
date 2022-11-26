7e807c194dea32196428bd4957b14bcfe0b14c42
GeraDot4815 <sanya.ravno.top@gmail.com>
2022-11-27 00:52:34 +0300

    Исправил баги врага, сделал отдачу от удара как в Hollow knight

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,7 +8,15 @@ public class Enemy : Creature
         if (GlobalConstants.PlayerLayer == collision.gameObject.layer)
         {
             Player player = collision.gameObject.GetComponent<Player>();
-            player.GetDamage(damage, -player.GetComponent<Rigidbody2D>().velocity);
+            Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+            Vector2 dir;
+            if (rb.velocity != Vector2.zero) 
+            {
+                if (player.transform.position.x >= transform.position.x) dir = new Vector2(1, 1);
+                else dir = new Vector2(-1, 1);
+            }
+            else dir = new Vector2(transform.right.x, 1);
+            player.GetDamage(damage, dir);
         }
     }
 }
