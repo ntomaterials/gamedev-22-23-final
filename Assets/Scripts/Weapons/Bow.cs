0f837571ab888acd7b96085c8562b7fa494e4e5d
GeraDot4815 <sanya.ravno.top@gmail.com>
2023-02-06 21:15:24 +0300

    Пофиксил 1й и 2й уровень

--- Assets/Scripts/Weapons/Bow.cs
+++ Assets/Scripts/Weapons/Bow.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Bow : Weapon
 {
     [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Arrow projectilePrefab;
     [SerializeField] private AudioClip shotSound;
     public override void Fire()
     {
         if (reloadTime > 0f) return;
         if(shotSound!=null) audioSource.PlayOneShot(shotSound);
         reloadTime = reload;
-        GameObject pr = Instantiate(projectilePrefab, firePoint.position, transform.parent.rotation);
+        Arrow pr = Instantiate(projectilePrefab, firePoint.position, transform.parent.rotation);
+        pr.damage = this.damage;//“ак будет удобнее настраивать урон стрел. ћожно еще к лучнику похожее написать
     }
 }
