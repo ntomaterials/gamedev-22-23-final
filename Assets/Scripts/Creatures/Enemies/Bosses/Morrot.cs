1dc4a54d80147cc61dcb6b13474f5b89eee1f5ea
GeraDot4815 <sanya.ravno.top@gmail.com>
2023-03-10 21:36:27 +0300

    собрал все. Игрок немного бессмертный

--- Assets/Scripts/Creatures/Enemies/Bosses/Morrot.cs
+++ Assets/Scripts/Creatures/Enemies/Bosses/Morrot.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Linq;
-using UnityEditor.Build.Player;
+//using UnityEditor.Build.Player;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
